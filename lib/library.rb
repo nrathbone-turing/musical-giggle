251e5cd967c98e65b5cf9046a85fbb198a7d31d5
Nick Rathbone <nrathbone@joinhandshake.com>
2025-04-15 11:53:51 -0600

    Refactor Library class to add #checked_out_books method and correlated instance variable which gets initialized as an empty array, and gets populated when #checkout(book) gets called and the book is valid to check out (i.e., not missing from the library altogether or currently checkedout)

--- lib/library.rb
+++ lib/library.rb
@@ -6,6 +6,7 @@ class Library
     @name = name
     @books = []
     @authors = []
+    @checked_out_books = [[]]
   end
 
   def add_author(new_author)
@@ -23,10 +24,18 @@ class Library
 
   def checkout(book)
 
+    if @books.include?(book) == false
+      return false
+    elsif @checked_out_books.include?(book) == true
+      return false
+    else
+      @checked_out_books << book
+      return true
+    end
   end
 
   def checked_out_books
-
+    return @checked_out_books
   end
 
   def return(book)
