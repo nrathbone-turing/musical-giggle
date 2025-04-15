79b38110818f8b92d09f705b9c2d112e7ce2e30a
Nick Rathbone <nrathbone@joinhandshake.com>
2025-04-15 12:29:47 -0600

    Create and refactor tests for #checked_out_books and create tests for #return and add some puts messages into the console for flair based on the actions taken

--- lib/library.rb
+++ lib/library.rb
@@ -6,7 +6,7 @@ class Library
     @name = name
     @books = []
     @authors = []
-    @checked_out_books = [[]]
+    @checked_out_books = []
   end
 
   def add_author(new_author)
@@ -25,11 +25,14 @@ class Library
   def checkout(book)
 
     if @books.include?(book) == false
+      puts "This book is not available at this library. Please contact the front desk for assistance."
       return false
-    elsif @checked_out_books.include?(book) == true
+    elsif @checked_out_books.include?(book)
+      puts "This book has already been checked out. Please contact the front desk for assistance."
       return false
     else
       @checked_out_books << book
+      puts "You have successfully checked out #{book.title}. It will need to be returned or extended in the next 14 days to avoid a fee."
       return true
     end
   end
@@ -39,7 +42,12 @@ class Library
   end
 
   def return(book)
-
+    if @checked_out_books.include?(book)
+      @checked_out_books.delete(book)
+      puts "#{book.title} is no longer checked out and has been returned successfully." 
+    else
+      puts "You cannot return a book that has not been checked out. Please contact the front desk for assistance."
+    end
   end
 
 
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -8,6 +8,15 @@ RSpec.describe Library do
 
   before(:each) do
     @dpl = Library.new("Denver Public Library")
+
+    @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    @harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+
+    @jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")    
+    @professor = @charlotte_bronte.write("The Professor", "1857")
+    @villette = @charlotte_bronte.write("Villette", "1853")
+
+    @mockingbird = @harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
   end
 
   describe '#initialize' do
@@ -31,48 +40,27 @@ RSpec.describe Library do
 
   describe '#add_author' do
 
-    it 'can receieve author information and books they have written'do
-      charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
-      harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
-     
-      @dpl.add_author(charlotte_bronte)
-      @dpl.add_author(harper_lee)
+    it 'can receieve author information and books they have written' do
+      @dpl.add_author(@charlotte_bronte)
+      @dpl.add_author(@harper_lee)
 
-      expect(@dpl.authors).to eq([charlotte_bronte, harper_lee])
+      expect(@dpl.authors).to eq([@charlotte_bronte, @harper_lee])
     end
 
-    it 'can add books written by those authors into an array of book objects' do
-      charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
-      jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")    
-      professor = charlotte_bronte.write("The Professor", "1857")
-      villette = charlotte_bronte.write("Villette", "1853")
+    it 'can add books written by those authors into an array of book objects' do    
+      @dpl.add_author(@charlotte_bronte)
+      @dpl.add_author(@harper_lee)
     
-      harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
-      mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
-    
-      @dpl.add_author(charlotte_bronte)
-      @dpl.add_author(harper_lee)
-    
-      expect(@dpl.books).to eq([jane_eyre, professor, villette, mockingbird])
+      expect(@dpl.books).to eq([@jane_eyre, @professor, @villette, @mockingbird])
     end
   end
 
   describe '#publication_time_frame' do
 
-    before(:each) do
-      @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
-      @jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")    
-      @professor = @charlotte_bronte.write("The Professor", "1857")
-      @villette = @charlotte_bronte.write("Villette", "1853")
-    
-      @harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
-      @mockingbird = @harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
-
+    it 'returns a key/value pair for the earliest publication year of an authors books' do
       @dpl.add_author(@charlotte_bronte)
       @dpl.add_author(@harper_lee)
-    end
 
-    it 'returns a key/value pair for the earliest publication year of an authors books' do
       expected_output = @dpl.publication_time_frame(@charlotte_bronte)
       expect(expected_output[:start]).to eq("1847")
 
@@ -82,6 +70,9 @@ RSpec.describe Library do
     end
 
     it 'returns a key/value pair for the latest publication year of an authors books' do
+      @dpl.add_author(@charlotte_bronte)
+      @dpl.add_author(@harper_lee)
+      
       expected_output = @dpl.publication_time_frame(@charlotte_bronte)
       expect(expected_output[:end]).to eq("1857")
 
@@ -90,22 +81,15 @@ RSpec.describe Library do
     end
 
     it 'returns a hash with both of the key/value pairs which points to the first and last publication years for an author' do
+      @dpl.add_author(@charlotte_bronte)
+      @dpl.add_author(@harper_lee)
+
       expect(@dpl.publication_time_frame(@charlotte_bronte)).to eq({:start=>"1847", :end=>"1857"})
       expect(@dpl.publication_time_frame(@harper_lee)).to eq({:start=>"1960", :end=>"1960"})
     end
   end
 
-  describe 'checkout books logic' do
-
-    before(:each) do 
-      @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
-      @jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")    
-      @professor = @charlotte_bronte.write("The Professor", "1857")
-      @villette = @charlotte_bronte.write("Villette", "1853")
-    
-      @harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
-      @mockingbird = @harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
-    end
+  describe '#checkout(book)' do
 
     it 'can checkout a book if it exists and is not already checked out' do
       @dpl.add_author(@charlotte_bronte)
@@ -127,9 +111,54 @@ RSpec.describe Library do
     
       expect(@dpl.checkout(@jane_eyre)).to eq(false)
     end
+  end
     
+  describe '#checked_out_books' do
 
+    it 'starts out with no checked out books' do
+      expect(@dpl.checked_out_books).to eq([])
+    end
+    
+    it 'adds a book to this array after it is successfully checked out' do
+      @dpl.add_author(@charlotte_bronte)
+      book = @jane_eyre
+      
+      @dpl.checkout(book)
+    
+      expect(@dpl.checked_out_books).to eq([@jane_eyre])
+    end
+    
+    it 'removes a book from checked_out_books when it is returned' do
+      @dpl.add_author(@charlotte_bronte)
+      book = @jane_eyre
+      
+      @dpl.checkout(book)
+      @dpl.return(book)
+    
+      expect(@dpl.checked_out_books).not_to include(@jane_eyre)
+    end
+  end
 
+  describe '#return(book)' do
+
+    it 'returns a confirmation message when a book is successfully returned' do
+      @dpl.add_author(@charlotte_bronte)
+      book = @jane_eyre
+      @dpl.checkout(book)
+    
+      @dpl.return(book)
+      
+      expect(@dpl.checked_out_books).to not_include(book)
+    end
+    
+    it 'returns an error message when trying to return a book that was never checked out' do
+      @dpl.add_author(@charlotte_bronte)
+      book = @villette
+      @dpl.checkout(book)
+      @dpl.return(book)
+
+      expect(@dpl.return(book)).to eq(nil)
+    end
   end
 
 end
