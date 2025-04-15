e03157e3fc9fb7c93a59a68d8c356f6ed392146d
Nick Rathbone <nrathbone@joinhandshake.com>
2025-04-15 11:47:04 -0600

    create tests for checkout method

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -10,23 +10,27 @@ RSpec.describe Library do
     @dpl = Library.new("Denver Public Library")
   end
 
-  it 'exists' do
-    expect(@dpl).to be_a(Library)
-  end
+  describe '#initialize' do
 
-  it 'has a name of Denver Public Library' do
-    expect(@dpl.name).to eq("Denver Public Library")
-  end
+    it 'exists' do
+      expect(@dpl).to be_a(Library)
+    end
 
-  it 'starts with no books' do
-    expect(@dpl.books).to eq([])
-  end
-
-  it 'starts with no authors' do
-    expect(@dpl.authors).to eq([])
+    it 'has a name of Denver Public Library' do
+      expect(@dpl.name).to eq("Denver Public Library")
+    end
+  
+    it 'starts with no books' do
+      expect(@dpl.books).to eq([])
+    end
+  
+    it 'starts with no authors' do
+      expect(@dpl.authors).to eq([])
+    end
   end
 
   describe '#add_author' do
+
     it 'can receieve author information and books they have written'do
       charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
       harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
@@ -91,5 +95,41 @@ RSpec.describe Library do
     end
   end
 
+  describe 'checkout books logic' do
+
+    before(:each) do 
+      @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+      @jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")    
+      @professor = @charlotte_bronte.write("The Professor", "1857")
+      @villette = @charlotte_bronte.write("Villette", "1853")
+    
+      @harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+      @mockingbird = @harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+    end
+
+    it 'can checkout a book if it exists and is not already checked out' do
+      @dpl.add_author(@charlotte_bronte)
+      book = @jane_eyre
+    
+      expect(@dpl.checkout(book)).to eq(true)
+      expect(@dpl.checked_out_books).to include(book)
+    end
+    
+    it 'cannot checkout a book that does not exist in the library' do
+      missing_book = Book.new({author_first_name: "Megan", author_last_name: "McMahon", title: "All Roads Lead to Nowhere", publication_date: "2024"})
+      expect(@dpl.checkout(missing_book)).to eq(false)
+    end
+    
+    it 'cannot checkout a book that is already checked out' do
+      @dpl.add_author(@charlotte_bronte)
+      book = @jane_eyre
+      @dpl.checkout(book)
+    
+      expect(@dpl.checkout(@jane_eyre)).to eq(false)
+    end
+    
+
+
+  end
 
 end
